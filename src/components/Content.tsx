fb2edf7331b72eaf35cdd676d1530f8770a1eae5
ssillazz <sillazwd@gmail.com>
2021-03-23 21:07:10 -0300

    Projeto finalizado!

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,35 @@
-export function Content() {
+import { MovieCard } from '../components/MovieCard';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export function Content(props:{
+  movies:Array<MovieProps>,
+  selectedGenreTitle: string,
+}) {
   // Complete aqui
+
+  return(
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {props.selectedGenreTitle}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {props.movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
 }
